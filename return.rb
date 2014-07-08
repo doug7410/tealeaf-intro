324faa8e98473b7ab2e92b5484b1c8e1dab1d50e
Gustavo Pares <gustavoanalytics@gmail.com>
2014-07-08 02:30:34 -0700

    Added variable exercises and changed the_basics to the_basic_exercises

---
+++ return.rb
@@ -0,0 +1,6 @@
+def add_three(number)
+  return number + 3 # return ends the flow and the next line of code is not interpreted
+  number + 10
+end
+
+p add_three(5)
---
+++ say.rb
@@ -0,0 +1,15 @@
+def say(words='hello')
+  puts words + '.'
+end
+
+say("hello")
+say("hi")
+say
+
+def some_method(number)
+  number = 7
+end
+
+a = 5
+some_method(a)
+puts a
