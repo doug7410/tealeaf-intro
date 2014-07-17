32b6ffe823cb586d2b889b604b5615a945ac9328
stay creative design <gustavoanalytics@gmail.com>
2014-07-17 12:26:52 -0700

    Reviewed video and learned to keep user data integrity for security reasons as well as tracking

--- calculator/print.rb
+++ calculator/print.rb
@@ -4,32 +4,39 @@ puts "Welcome to the awesome simple calculator where calculations are made with 
 puts "======================================================================="
 
 while
+  def say(msg)
+    print "\n=> #{msg}"
+  end
+
   operation = ''
-  puts "\nWhat do want to do? \n\n"
-  puts "1) add   2) subtract   3) divide   4) multiply   5) exit program"
+  puts "\n\nWhat do want to do? \n\n"
+  puts "1) add   2) subtract   3) multiply   4) divide   5) exit program"
   print "\nPlease select mathematical operation >> "
-  operation = gets.chomp.to_i
-  if operation == 5
+  operation = gets.chomp
+  if operation.to_i == 5
     puts "\n\nThank you for using awesome simple calculator!\n\n"
     break
-  elsif operation >= 6 || operation <= 0
+  elsif operation.to_i >= 6 || operation.to_i <= 0
     puts "\nplease select a correct number for mathematical operation!"
     next
   end
-  print "\nPlease select first number >> "  
-  num1 = gets.chomp.to_i
-  print "\nPlease select second number >> "
-  num2 = gets.chomp.to_i      
+  say("Please select first number  >> ")  
+  num1 = gets.chomp
+  say("Please select second number >> ")
+  num2 = gets.chomp      
 
-  case operation
+  case operation.to_i
   when 1
-    puts "\n#{num1} + #{num2} = #{num1 + num2}"
+    result =  "#{num1} + #{num2} = #{num1.to_i + num2.to_i}"
   when 2
-    puts "\n#{num1} - #{num2} = #{num1 - num2}"
+    result =  "#{num1} - #{num2} = #{num1.to_i - num2.to_i}"
   when 3
-    puts "\n#{num1} x #{num2} = #{num1 * num2}"
+    result =  "#{num1} x #{num2} = #{num1.to_i * num2.to_i}"
   when 4 
-    puts "\n#{num1} / #{num2} = #{num1.to_f / num2.to_f } "   
+    result =  "#{num1} / #{num2} = #{num1.to_f / num2.to_f } "   
   end
+  say("This result is #{result}")
 end
 
+# looked at video for lesson 1 part 2
+# keep data integrity for user ie. if the user enters something weird we are able to track that
